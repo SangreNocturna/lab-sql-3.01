fd413b29fed1067b143f0e11b88b542de2d4fc62
SangreNocturna <jtafernandez@gmail.com>
2022-09-19 18:06:02 +0200

    sql-lab_3_0_1.sql

--- sql-lab_3_0_1.sql
+++ sql-lab_3_0_1.sql
@@ -1,4 +1,5 @@
 USE sakila;
+-- Activity 1
 
 -- 1. Drop column picture from staff
 SELECT * from sakila.staff;
@@ -10,4 +11,23 @@ SELECT * from sakila.staff;
 
 -- 2. A new person is hired to help Jon. Her name is TAMMY SANDERS, and she is a customer. 
 -- Update the database accordingly.
+
+insert into sakila.staff(first_name, last_name, email, address_id, store_id, username)
+values('TAMMY','SANDERS', 'TAMMY.SANDERS@sakilacustomer.org', 79, 2, 'tammy');
+select * from sakila.staff;
+
+
+SELECT first_name, last_name from sakila.staff;
+
+-- Activity 2
+
+-- 1. Drop the film.text table
+-- 2. Change phone data type from VARCHAR to INT
+-- 3. Add phone number to the staff
+-- 4. In sakila.film, title is reduced from 128 to 50 VARCHAR
+
+SELECT phone from sakila.address;
+
+
+
 
